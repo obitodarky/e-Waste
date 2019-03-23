2fe1b1099d7c44cc1c94312dab0e4da4e76b3b10
Harsh <deadspaceharsh@gmail.com>
2019-03-23 22:45:01 +0530

    removed viewdidload

--- e-Waste/Controller/ViewController.swift
+++ e-Waste/Controller/ViewController.swift
@@ -10,10 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
     @IBOutlet var email: UITextField!
     @IBOutlet var password: UITextField!
