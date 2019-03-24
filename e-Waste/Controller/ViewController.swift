01c32e1a53bd8f62940a3762c68f2c1481882184
Harsh <deadspaceharsh@gmail.com>
2019-03-24 16:44:55 +0530

    table view added

--- e-Waste/Controller/ViewController.swift
+++ e-Waste/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
 
     @IBOutlet var email: UITextField!
---
+++ e-Waste/Controller/WasteTypeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WasteTypeViewController.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 24/03/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class WasteTypeViewController: ViewController,UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = "Bacon"
+        
+        return cell
+    }
+    
+    
+}
