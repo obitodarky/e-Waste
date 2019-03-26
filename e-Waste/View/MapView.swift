225b0894eccfada857749ff66effa591b9199ca1
Harsh <deadspaceharsh@gmail.com>
2019-03-26 16:28:19 +0530

    file changes and mapkit changes

---
+++ e-Waste/View/MapView.swift
@@ -0,0 +1,15 @@
+//
+//  MapView.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 26/03/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapView: ViewController {
+    
+    
+}
