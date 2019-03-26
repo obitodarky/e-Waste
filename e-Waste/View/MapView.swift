2af37e3dcd4cac95d1e80964d7a6aae32d5d1ece
Harsh <deadspaceharsh@gmail.com>
2019-03-26 17:35:38 +0530

    added central location and zoomed map

--- e-Waste/View/MapView.swift
+++ e-Waste/View/MapView.swift
@@ -15,14 +15,45 @@ class MapView: ViewController {
     
     @IBOutlet var areaMapView: MKMapView!
     
+    var centralLocation: Double = 10000
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setLocation()
     }
     
+    func setLocationManager(){
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerLocation(){
+    
+        if let location = locationManager.location?.coordinate{
+            
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: centralLocation , longitudinalMeters: centralLocation)
+            areaMapView.setRegion(region, animated: true)
+        }
+    }
+    
     func setLocation() {
         
         areaMapView.showsUserLocation = true
+        centerLocation()
         
     }
+}
+
+extension MapView: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //code
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //code
+    }
+    
 }
