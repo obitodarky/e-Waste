133395fd81ab889ca4a860ee19c828d60c8694f7
Harsh <deadspaceharsh@gmail.com>
2019-03-29 09:53:24 +0530

    added new view for photo

---
+++ e-Waste/Controller/ReportWasteViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ReportWasteViewController.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 29/03/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ReportWasteViewController: ViewController {
+    
+    @IBOutlet weak var wastePhoto: UIImageView!
+    
+    @IBAction func takePhoto(_sender: Any){
+        
+        
+    }
+    
+}
