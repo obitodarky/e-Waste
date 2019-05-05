fdd7b37a94b389f534d0737b73dededc8c2482ad
Harsh <deadspaceharsh@gmail.com>
2019-05-05 08:38:35 +0530

    tableview datasource,delegates

--- e-Waste/Controller/ScrapViewController.swift
+++ e-Waste/Controller/ScrapViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class ScrapViewController: UIViewController {
+class ScrapViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
     
 
