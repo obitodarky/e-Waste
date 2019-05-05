c43d8b90be907ac40ceff680c48b0840b3e5fded
Harsh <deadspaceharsh@gmail.com>
2019-05-05 08:33:18 +0530

    scrap controller

---
+++ e-Waste/Controller/ScrapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScrapViewController.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 05/05/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ScrapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
