83fec3ab770f2819d5a50ec6b95bdb17c5546be0
Harsh <deadspaceharsh@gmail.com>
2019-05-14 13:02:30 +0530

    user register

---
+++ e-Waste/Controller/UserReigsterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserReigsterViewController.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 14/05/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class UserReigsterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
