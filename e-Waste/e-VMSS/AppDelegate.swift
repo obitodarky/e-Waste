8670a6c21c7470e7adaa3c63511723eb4a298ee9
Harsh <deadspaceharsh@gmail.com>
2019-05-14 12:47:41 +0530

    saves to firebase

--- e-Waste/e-VMSS/AppDelegate.swift
+++ e-Waste/e-VMSS/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("Sample Value")
         return true
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
