9ea34299b081f955e08cc1f1e8a708ca06f7faa9
Harsh <deadspaceharsh@gmail.com>
2019-05-16 10:55:19 +0530

    added animations

--- e-Waste/Controller/ViewController.swift
+++ e-Waste/Controller/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController{
 
     @IBOutlet var email: UITextField!
     @IBOutlet var password: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 15
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: password.center.x - 10, y: password.center.y) )
+        animation.toValue = NSValue(cgPoint: CGPoint(x: password.center.x + 10, y: password.center.y))
+        
+        password.layer.add(animation, forKey: "position")
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
