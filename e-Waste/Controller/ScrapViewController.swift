a7ec83c419b354a929a1503a2a8a8755c4d1b84e
Harsh <deadspaceharsh@gmail.com>
2019-05-16 13:05:22 +0530

    fixed login bug

--- e-Waste/Controller/ScrapViewController.swift
+++ e-Waste/Controller/ScrapViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
+
+
 class ScrapViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
 
     @IBOutlet var scrapTableView: UITableView!
+    let companyInfo:[String:String] = ["Electronics": "electronics"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,14 +22,14 @@ class ScrapViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return companyInfo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = scrapTableView.dequeueReusableCell(withIdentifier: "waste", for: indexPath)
         
-        
+        cell.textLabel?.text = "\(companyInfo)"
         return cell
         
     }
--- e-Waste/Controller/ViewController.swift
+++ e-Waste/Controller/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        incorrectLogin.text = ""
+        
     }
     
     func wrongLogIn(){
