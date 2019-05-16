6c11f82cba8eb65b3fda480f525b6e5bdbe97418
Harsh <deadspaceharsh@gmail.com>
2019-05-16 13:10:48 +0530

    added tableviewcell

---
+++ e-Waste/View/CompanyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CompanyTableViewCell.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 16/05/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class CompanyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
