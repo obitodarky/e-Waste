4642f8c6b76dcd59db77cd1f5e17f01b907c6cc0
Harsh <deadspaceharsh@gmail.com>
2019-05-17 13:26:35 +0530

    read data from firebase

--- e-Waste/Controller/WasteTypeViewController.swift
+++ e-Waste/Controller/WasteTypeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 let wasteArray = ["Vegetable Peels ", "Mop Stick ", "Mosquito Repellent Refill Bottles " ,"Fruit Peels ", "Used Mop Cloth ", "Mosquito Repellent Mats ", "Rotten Vegetables ","Toilet Cleaning Brush ","Used Odonil","Rotten Fruits    ","Brush and Scrubs for Cleaning", "Expired Medicines or Medicine Bottles","Left Over Food ","Soap Covers ","Used Syringes","Mango Seeds  ","Chocolate Wrappers ","Diapers and sanitary pads","Used Tea Bags ","Butter Paper ","Injection Bottles","Used Coffee Powder from Filter ","Milk Covers ","Compact Fluorescent Light(CFL)","Egg Shells ","Ghee/Oil Packets ","Used Cooking oil","Rotten Eggs ","Oil Cans ","Bottles or cans of Mosquito Sprays","Coconut Shells ","Newspaper ","Fluorescent","Tender Coconut Shells    ","Used paper Pieces ","Button Cells","Used Leaves and Flowers  ","Old Posts ","Hospital waste ","Spoiled Spices ","Broken Stationary ","Bottles or cans of Insecticide Sprays","Floor Sweeping Dust ","Used Razor Blades ","Thermometers","Meat and Non-Veg Remains ","Empty Shampoo Bottle  ","Batteries","Bones","Empty Perfume Bottle","Used Condoms","Left Over Pet Food  ","Thermocol","Chemical container of appliances","Garden Leaves","Broken Glass  ","Used Cotton and Bandage","Dried Flowers  ","Plastic Items  ","Sterile gauge","Weed  ","Aluminum Cans  ","Motor Oil","Bread Crusts  ","Aluminum Foils  ","Cell Phones"]
 
@@ -14,18 +15,35 @@ var final_index = 0
 
 class WasteTypeViewController: ViewController,UITableViewDataSource,UITableViewDelegate , UISearchBarDelegate, UISearchDisplayDelegate {
     
+    var ref: DatabaseReference?
+    var databaseReference: DatabaseHandle?
+    
+    var wasteData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
         
     }
     
+    func getData(){
+        databaseReference = ref?.child("Vegetables").observe(.childAdded, with: { (snapshot) in
+            
+            let data = snapshot.value as? String
+            if let theData = data {
+                self.wasteData.append(theData)
+            }
+            
+            
+        })
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return wasteArray.count
+        return wasteData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
