68a2fd02ff8f507d16815b20033761fb87f27bf3
Harsh <deadspaceharsh@gmail.com>
2019-05-20 13:04:32 +0530

    build failing

---
+++ e-Waste/Controller/WasteDescriptionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WasteDescriptionViewController.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 20/05/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class WasteDescriptionViewController: UIViewController {
+
+    @IBOutlet var wasteImage: UIImageView!
+    @IBOutlet var wasteName: UILabel!
+    @IBOutlet var wasteType: UILabel!
+    @IBOutlet var wasteDescription: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
