88549fd19cca18a659ff34dd7ed6259615eb9259
Harsh <deadspaceharsh@gmail.com>
2019-05-20 14:44:04 +0530

    build - passing

--- e-Waste/Controller/WasteDescriptionViewController.swift
+++ e-Waste/Controller/WasteDescriptionViewController.swift
@@ -17,7 +17,7 @@ class WasteDescriptionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
