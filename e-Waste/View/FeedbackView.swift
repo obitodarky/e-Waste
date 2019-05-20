4cb7abb86ea430e8937843b3d218d10954105eae
Harsh <deadspaceharsh@gmail.com>
2019-05-20 16:23:56 +0530

    added feedback emoji

--- e-Waste/View/FeedbackView.swift
+++ e-Waste/View/FeedbackView.swift
@@ -25,7 +25,7 @@ class FeedbackView: UIViewController {
     
     @IBAction func submitFeedback(_ sender: Any) {
         if(feedbackMessage.text != ""){
-            feedbackSent.text = "Feedback sent successfuly"
+            feedbackSent.text = "✅Feedback sent successfuly"
         } else {
             feedbackSent.text = "Please write something"
         }
