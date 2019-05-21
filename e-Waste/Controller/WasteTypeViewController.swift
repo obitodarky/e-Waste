dffe1a60c77fb4d6ee0b956ca222aed765d1fa16
Harsh <deadspaceharsh@gmail.com>
2019-05-21 11:53:52 +0530

    added comments

--- e-Waste/Controller/WasteTypeViewController.swift
+++ e-Waste/Controller/WasteTypeViewController.swift
@@ -13,7 +13,7 @@ import FirebaseDatabase
 
 class WasteTypeViewController: ViewController,UITableViewDataSource,UITableViewDelegate , UISearchBarDelegate, UISearchDisplayDelegate {
 
-    
+    //MARK: variables and outlets
     var wasteArray = ["Vegetable Peels", "Mop Stick", "Mosquito Repellent Refill Bottles" ,"Fruit Peels ", "Used Mop Cloth", "Mosquito Repellent Mats", "Rotten Vegetables ","Toilet Cleaning Brush ","Used Odonil","Rotten Fruits","Brush and Scrubs for Cleaning", "Expired Medicines or Medicine Bottles","Left Over Food ","Soap Covers","Used Syringes","Mango Seeds","Chocolate Wrappers","Diapers and sanitary pads","Used Tea Bags","Butter Paper","Injection Bottles","Used Coffee Powder from Filter","Milk Covers","Compact Fluorescent Light(CFL)","Egg Shells","Ghee/Oil Packets","Used Cooking oil","Rotten Eggs","Oil Cans","Bottles or cans of Mosquito Sprays","Coconut Shells","Newspaper","Fluorescent","Tender Coconut Shells","Used paper Pieces","Button Cells","Used Leaves and Flowers","Old Posts","Hospital waste","Spoiled Spices","Broken Stationary","Bottles or cans of Insecticide Sprays","Floor Sweeping Dust","Used Razor Blades","Thermometers","Meat and Non-Veg Remains","Empty Shampoo Bottle","Batteries","Bones","Empty Perfume Bottle","Used Condoms","Left Over Pet Food","Thermocol","Chemical container of appliances","Garden Leaves","Broken Glass","Used Cotton and Bandage","Dried Flowers","Plastic Items","Sterile gauge","Weed","Aluminum Cans","Motor Oil","Bread Crusts","Aluminum Foils","Cell Phones"]
     
     
@@ -46,7 +46,7 @@ class WasteTypeViewController: ViewController,UITableViewDataSource,UITableViewD
 
         })
     }
-    
+    //MARK: table functions
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
