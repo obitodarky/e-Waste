68e733a251e07f78e706e25148bdc345b3030d5f
Harsh <deadspaceharsh@gmail.com>
2019-05-22 10:49:22 +0530

    org vc revamp

--- e-Waste/View/RegisterOrganizationView.swift
+++ e-Waste/View/RegisterOrganizationView.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class RegisterOrganizationView: UIViewController {
 
+    
+    @IBOutlet var organizationNumber: UITextField!
+    @IBOutlet var organizationImage: UIImageView!
+    @IBOutlet var organizationName: UITextField!
+    @IBOutlet var organizationDescription: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func organizationSubmitPressed(_ sender: Any) {
     }
     
 }
