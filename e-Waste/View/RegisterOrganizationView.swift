725454b46ce5fb1004c08941bf013d41b00dd973
Harsh <deadspaceharsh@gmail.com>
2019-05-23 11:00:09 +0530

    hidden fields

--- e-Waste/View/RegisterOrganizationView.swift
+++ e-Waste/View/RegisterOrganizationView.swift
@@ -19,11 +19,25 @@ class RegisterOrganizationView: UIViewController, UIImagePickerControllerDelegat
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        organizationDescription.text = ""
+        registrationStatus.text = ""
         organizationDescription.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
         organizationDescription.layer.borderWidth = 1.0;
         organizationDescription.layer.cornerRadius = 5.0;
 
+    }
+    
+    func wrongSubmit(){
+        
+        registrationStatus.frame.size.height = 22
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.07
+        animation.repeatCount = 3
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: registrationStatus.center.x - 10, y: registrationStatus.center.y) )
+        animation.toValue = NSValue(cgPoint: CGPoint(x: registrationStatus.center.x + 10, y: registrationStatus.center.y))
+        
+        registrationStatus.layer.add(animation, forKey: "position")
+        
     }
     
     //MARK: submit button
@@ -33,6 +47,7 @@ class RegisterOrganizationView: UIViewController, UIImagePickerControllerDelegat
             organizationNumber.text == ""       ||
             organizationImage.image  == nil
             ){
+            wrongSubmit()
             registrationStatus.text = "Please fill all fields"
         }
 
