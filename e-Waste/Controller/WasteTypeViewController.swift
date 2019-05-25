d5313bbdfeff0cb19778b539fe69af181a0600db
Harsh <deadspaceharsh@gmail.com>
2019-05-25 15:28:02 +0530

    removed waste array

--- e-Waste/Controller/WasteTypeViewController.swift
+++ e-Waste/Controller/WasteTypeViewController.swift
@@ -12,9 +12,6 @@ import FirebaseDatabase
 
 
 class WasteTypeViewController: ViewController,UITableViewDataSource,UITableViewDelegate , UISearchBarDelegate, UISearchDisplayDelegate {
-
-    //MARK: variables and outlets
-    var wasteArray = ["Vegetable Peels", "Mop Stick", "Mosquito Repellent Refill Bottles" ,"Fruit Peels", "Used Mop Cloth", "Mosquito Repellent Mats", "Rotten Vegetables","Toilet Cleaning Brush","Used Odonil","Rotten Fruits","Brush and Scrubs for Cleaning", "Expired Medicines or Medicine Bottles","Left Over Food ","Soap Covers","Used Syringes","Mango Seeds","Chocolate Wrappers","Diapers and sanitary pads","Used Tea Bags","Butter Paper","Injection Bottles","Used Coffee Powder from Filter","Milk Covers","Compact Fluorescent Light(CFL)","Egg Shells","Ghee/Oil Packets","Used Cooking oil","Rotten Eggs","Oil Cans","Bottles or cans of Mosquito Sprays","Coconut Shells","Newspaper","Fluorescent","Tender Coconut Shells","Used paper Pieces","Button Cells","Used Leaves and Flowers","Old Posts","Hospital waste","Spoiled Spices","Broken Stationary","Bottles or cans of Insecticide Sprays","Floor Sweeping Dust","Used Razor Blades","Thermometers","Meat and Non-Veg Remains","Empty Shampoo Bottle","Batteries","Bones","Empty Perfume Bottle","Used Condoms","Left Over Pet Food","Thermocol","Chemical container of appliances","Garden Leaves","Broken Glass","Used Cotton and Bandage","Dried Flowers","Plastic Items","Sterile gauge","Weed","Aluminum Cans","Motor Oil","Bread Crusts","Aluminum Foils","Cell Phones"]
     
     var wasteData = [Waste]()
     var wasteSearchArray = [String]()
@@ -29,7 +26,6 @@ class WasteTypeViewController: ViewController,UITableViewDataSource,UITableViewD
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        wasteArray.sort()
         
         tableView.delegate  = self
         tableView.dataSource = self
@@ -90,7 +86,7 @@ class WasteTypeViewController: ViewController,UITableViewDataSource,UITableViewD
     func fetchUser(){
         ref = Database.database().reference()
         ref?.observe(.childAdded, with: { (snapshot) in
-         
+            
             if let dictonary = snapshot.value as? NSDictionary{
                 let waste = Waste()
                 
@@ -98,6 +94,7 @@ class WasteTypeViewController: ViewController,UITableViewDataSource,UITableViewD
                 let description = dictonary["waste_description"] as? String ?? "Not found"
                 let type = dictonary["waste_type"] as? String ?? "Not found"
                 let image = dictonary["waste_image"] as? String ?? "Not found"
+                
                 waste.name = name
                 waste.waste_description = description
                 waste.waste_type = type
