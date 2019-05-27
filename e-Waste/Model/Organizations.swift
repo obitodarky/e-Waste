42ece21dc325e8b91918f42bfefa7afc04b97aa1
Harsh <deadspaceharsh@gmail.com>
2019-05-27 11:17:38 +0530

    added org model

---
+++ e-Waste/Model/Organizations.swift
@@ -0,0 +1,16 @@
+//
+//  Organizations.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 27/05/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class Organizations: NSObject {
+    var name: String?
+    var desc: String?
+    var number: String?
+    var image: String?
+}
