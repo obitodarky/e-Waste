621aed034aac1b2353c20d50f0c464d81464476b
Harsh <deadspaceharsh@gmail.com>
2019-05-28 13:07:35 +0530

    login design

---
+++ e-Waste/Views/LoginTextField.swift
@@ -0,0 +1,27 @@
+//
+//  LoginTextField.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 28/05/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LoginTextField: UITextField {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.borderColor = UIColor(white: 231/255, alpha: 1).cgColor
+        self.layer.borderWidth = 1
+        
+    }
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 8, dy: 7)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds: bounds)
+    }
+}
