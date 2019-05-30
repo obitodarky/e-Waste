86f61e21f5063191fb8044a808ead20d58d6e147
Harsh <deadspaceharsh@gmail.com>
2019-05-30 11:57:58 +0530

    changed colorline

--- e-Waste/Views/MapView.swift
+++ e-Waste/Views/MapView.swift
@@ -114,10 +114,28 @@ extension MapView: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
-        renderer.strokeColor = .green
+        renderer.strokeColor = UIColor(rgb: 0x71DB90)
         return renderer
     }
     
 }
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+
+}
 
