ef0883d83ba0f179aff24669b2a61f34e9905b5d
Harsh <deadspaceharsh@gmail.com>
2019-05-30 16:07:07 +0530

    removed design

--- e-Waste/Views/CellDesignView.swift
+++ e-Waste/Views/CellDesignView.swift
@@ -13,18 +13,12 @@ class CellDesignView: UIView {
 
     @IBInspectable var cornerRadius: CGFloat = 0
     @IBInspectable var shadowColor: UIColor? = UIColor.black
-    @IBInspectable let shadowOffSetWidth: Int = 0
-    @IBInspectable let shadowOffSetHeight: Int = 1
+
     
     @IBInspectable var shadowOpacity: Float = 0.2
     
     override func layoutSubviews() {
         layer.shadowColor = shadowColor?.cgColor
-        layer.shadowOffset = CGSize(width: shadowOffSetWidth, height: shadowOffSetHeight)
-        
-        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
-        layer.shadowPath = shadowPath.cgPath
-        layer.shadowOpacity = shadowOpacity
     }
 
 }
