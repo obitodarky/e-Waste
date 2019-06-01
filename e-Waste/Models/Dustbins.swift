fab58538675c5936540d3c4c1775cba07c0eced9
Harsh <deadspaceharsh@gmail.com>
2019-06-01 11:34:58 +0530

    added dustbin module

---
+++ e-Waste/Models/Dustbins.swift
@@ -0,0 +1,14 @@
+//
+//  Dustbins.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 01/06/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class Dustbins: NSObject {
+    var latitude: String?
+    var longitude: String?
+}
