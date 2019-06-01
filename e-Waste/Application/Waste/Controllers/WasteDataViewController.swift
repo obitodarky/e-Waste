0cfc7af8a98e82c18c056b838bf8c32c65aa6cdf
Harsh <deadspaceharsh@gmail.com>
2019-06-01 15:09:52 +0530

    added wastedata vc

---
+++ e-Waste/Application/Waste/Controllers/WasteDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WasteDataViewController.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 01/06/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class WasteDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
