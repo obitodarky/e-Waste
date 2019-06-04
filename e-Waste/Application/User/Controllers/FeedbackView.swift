b1c99e9af87705488fa8ec4209cff1a9717b577a
Harsh <deadspaceharsh@gmail.com>
2019-06-04 10:46:15 +0530

    shadows

--- e-Waste/Application/User/Controllers/FeedbackView.swift
+++ e-Waste/Application/User/Controllers/FeedbackView.swift
@@ -30,8 +30,8 @@ class FeedbackView: UIViewController {
         super.viewDidLoad()
         
         feedbackSent.text = ""
-        feedbackMessage.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
-        feedbackMessage.layer.borderWidth = 1.0;
+        feedbackMessage.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 3.0).cgColor
+        feedbackMessage.layer.borderWidth = 2.0;
         feedbackMessage.layer.cornerRadius = 5.0;
     }
     
