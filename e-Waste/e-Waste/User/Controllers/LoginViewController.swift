675137ce649d98f4419c0f341608fb066710a325
Harsh <deadspaceharsh@gmail.com>
2019-06-07 17:18:20 +0530

    color animation

--- e-Waste/e-Waste/User/Controllers/LoginViewController.swift
+++ e-Waste/e-Waste/User/Controllers/LoginViewController.swift
@@ -50,12 +50,12 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate{
         
         colorArray.append((color1: #colorLiteral(red: 0.5033842325, green: 0.8740338683, blue: 0.6319764256, alpha: 0.8470588235), color2: #colorLiteral(red: 0.3411764801, green: 0.8529547339, blue: 0.4490480466, alpha: 1)))
         colorArray.append((color1: #colorLiteral(red: 0.3411764801, green: 0.8529547339, blue: 0.4490480466, alpha: 1), color2: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), color2: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1), color2: #colorLiteral(red: 0.2392156869, green: 0.8018642754, blue: 0.9686274529, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.2392156869, green: 0.8018642754, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), color2: #colorLiteral(red: 0.4044704911, green: 0.8740338683, blue: 1, alpha: 0.8470588235)))
-        colorArray.append((color1: #colorLiteral(red: 0.4044704911, green: 0.8740338683, blue: 1, alpha: 0.8470588235), color2: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
-        colorArray.append((color1: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1), color2: #colorLiteral(red: 0.4164567924, green: 0.8740338683, blue: 0.4390986431, alpha: 0.8470588235)))
+        colorArray.append((color1: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), color2: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+        colorArray.append((color1: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), color2: #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)))
+        colorArray.append((color1: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1), color2: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+        colorArray.append((color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
+        colorArray.append((color1: #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1), color2: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
+        colorArray.append((color1: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1), color2: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().signIn()
         animateBackgroundColor()
@@ -69,7 +69,7 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate{
             colorArrayIndex += 1
         }
         
-        UIView.transition(with: gradientView, duration: 2, options: [.transitionCrossDissolve], animations: {
+        UIView.transition(with: gradientView, duration: 4, options: [.transitionCrossDissolve], animations: {
             self.gradientView.firstColor = self.colorArray[self.colorArrayIndex].color1
             self.gradientView.secondColor = self.colorArray[self.colorArrayIndex].color2
         }) { (sucess) in
