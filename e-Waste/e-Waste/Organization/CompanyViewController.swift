f9c253b823cee1abe6ba443d6aa653ff4f3dfaa5
Harsh <deadspaceharsh@gmail.com>
2019-06-08 10:50:57 +0530

    removed tablecell

---
+++ e-Waste/e-Waste/Organization/CompanyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CompanyViewController.swift
+//  e-VMSS
+//
+//  Created by Harsh Mehta on 08/06/19.
+//  Copyright © 2019 Harsh Mehta. All rights reserved.
+//
+
+import UIKit
+
+class CompanyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
