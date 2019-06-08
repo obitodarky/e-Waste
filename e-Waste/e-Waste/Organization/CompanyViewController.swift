7cdc2a3f1fcd45659ac28f3bb76430eea9f709fd
Harsh <deadspaceharsh@gmail.com>
2019-06-08 11:40:53 +0530

    outlets

--- e-Waste/e-Waste/Organization/CompanyViewController.swift
+++ e-Waste/e-Waste/Organization/CompanyViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class CompanyViewController: UIViewController {
 
+    @IBOutlet var gradientView: UIViewX!
+    @IBOutlet var companyCollectionView: UICollectionView!
+    var colorArray: [(color1: UIColor,color2: UIColor)] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
-
-
 
 }
