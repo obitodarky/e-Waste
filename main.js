8653867935e7cae57248c3c0e16caaa071ed58c7
Harsh Mehta <deadspaceharsh@gmail.com>
2019-06-20 12:44:54 +0530

    gets value from firebase

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 var app_name = angular.module('app_name', []);
 
 app_name.controller('ctrl1', function($scope){
-    $scope.updateValue = function() {
-        $scope.final_name = $scope.name;
-    }
+    
+    $scope.items = [];
+    
+    var ref = database.ref('Items/');
+    ref.on('value', function(snapshot){
+        //console.log(snapshot.val());
+        var data = snapshot.val();
+        var keys = Object.keys(data);
+        for(var i = 0; i < keys.length; i++){
+            var k = keys[i]
+            var item = data[k].name;
+            $scope.items.push(item);
+        }
+    });
+    
+    
 
 });
