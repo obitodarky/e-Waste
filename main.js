9ee304eeeedfe479819992b1cc002d591e5d6b12
Harsh Mehta <deadspaceharsh@gmail.com>
2019-06-20 18:17:41 +0530

    writes data

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ app_name.controller('ctrl1', function($scope){
     
     $scope.items = [];
     
-    var ref = database.ref('Items/');
-    ref.on('value', function(snapshot){
-        //console.log(snapshot.val());
-        var data = snapshot.val();
-        var keys = Object.keys(data);
-        for(var i = 0; i < keys.length; i++){
-            var k = keys[i]
-            var item = data[k].name;
-            $scope.items.push(item);
-        }
-    });
-    
-    
+   $scope.writeData = function() {
+        firebase.database().ref('Items/' + $scope.name).set({
+            name: $scope.name,
+            waste_description: $scope.desc,
+            waste_type: $scope.type
+            });
+    }
 
+    var init = function(){
+        var ref = database.ref('Items/');
+        ref.on('value', function(snapshot){
+            console.log(snapshot.val());
+            var data = snapshot.val();
+            var keys = Object.keys(data);
+            for(var i = 0; i < keys.length; i++){
+                var k = keys[i]
+                var item = data[k].name;
+                $scope.items.push(item);
+            }
+        });
+        
+    }
+
+    init();
 });
